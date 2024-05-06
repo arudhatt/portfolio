54e1e3b0fd714a09b58e4c6a3731b173e1bbe896
Arminder Chahal <arudhatt@gmail.com>
2024-05-06 15:44:06 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
 import Image from "next/image";
+
+  const MyImage = () => {
+    return (
+      <Image
+        src="/image.jpg"
+        alt="This is an image"
+      />
+    );
+  }
+
 import { About } from "@/components/about"
 
 export default function Home() {
