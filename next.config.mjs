78049638574efd6ea763c60b46fca808f65a4ab1
Arminder Chahal <arudhatt@gmail.com>
2024-05-06 15:10:14 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,6 @@ const nextConfig = {
     basePath: "/portfolio",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
   };
   
   module.exports = nextConfig;
