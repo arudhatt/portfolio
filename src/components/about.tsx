b76befe3ab2a1e37cd6512d6479825bc28a3c518
Arminder Chahal <arudhatt@gmail.com>
2024-05-06 19:42:25 -0600

    Update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -37,7 +37,7 @@ export function About() {
             </p>
           </div>
           <img
-            src="./blob/main/public/profile.jpg"
+            src="/profile.jpg"
             alt="Arminder Kaur"
             className="rounded-full w-32 h-32 md:w-40 md:h-40"
             style={{
