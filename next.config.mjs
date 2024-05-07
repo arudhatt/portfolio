0b8bab92c13e18e54e040fafea355ca947793c0e
Arminder Chahal <arudhatt@gmail.com>
2024-05-06 18:43:22 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,6 @@ const nextConfig = {
     basePath: "/portfolio",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-  };
+  }
   
-  module.exports = nextConfig;
+  export default nextConfig;
